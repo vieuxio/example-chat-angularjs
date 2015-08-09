399fce7ed31533d588d453612bdd77ef7ee19c25
Murat Çorlu <muratcorlu@gmail.com>
2015-08-10 01:33:05 +0300

    Lazy responses from ChatUndertaker for demo purposes

--- src/ChatUnion/ChatUndertaker.js
+++ src/ChatUnion/ChatUndertaker.js
@@ -1,17 +1,25 @@
 'use strict';
 
 angular.module('ChatUnion')
-    .service('ChatUndertaker', function ($http) {
+    .service('ChatUndertaker', function ($http, $q) {
+        // For demo purposes
+        var lazyResponse = function (promise, duration) {
+            duration = duration || 300;
+
+            return $q(function (resolve) {
+                promise.then(resolve);
+            });
+        };
 
         this.getThreads = function () {
-            return $http.get('/api/threads');
+            return lazyResponse($http.get('/api/threads'));
         };
 
         this.getUpdates = function () {
-            return $http.get('/api/updates');
+            return lazyResponse($http.get('/api/updates'));
         };
 
         this.getOwner = function () {
-            return $http.get('/api/owner');
+            return lazyResponse($http.get('/api/owner'));
         };
     });
